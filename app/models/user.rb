2087b2d041e156d5079d4836047fe25be5b2345c
shimura <shimura21ngs@gmail.com>
2022-09-04 22:11:49 +0900

    Add user following"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,29 @@
 class User < ApplicationRecord
+
+  # micropostsテーブルとの関連付け
   has_many :microposts, dependent: :destroy
+
+  # Active_relationshipsテーブルとの関連付け
+  has_many :active_relationships,
+    class_name:   "Relationship",
+    foreign_key:  "follower_id",
+    dependent:    :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  # Passive_relationshipsテーブルとの関連付け
+  has_many :passive_relationships,
+    class_name: "Relationship",
+    foreign_key: "followed_id",
+    dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
+validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
@@ -72,8 +90,33 @@ class User < ApplicationRecord
   # 試作feedの定義
   # 完全な実装は次章の「ユーザーをフォローする」を参照
   def feed
-    Micropost.where("user_id = ?", id)
+    # Micropost.where(
+    #   "user_id IN (:following_ids) OR user_id = :user_id",
+    #   following_ids: following_ids,
+    #   user_id: id
+    # )
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
 
   private
 
@@ -87,4 +130,7 @@ class User < ApplicationRecord
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
+    
+
 end
