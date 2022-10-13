5faeefdb304ebc4e8428f86b526c57e1bd947600
shimura <shimura21ngs@gmail.com>
2022-10-13 17:08:00 +0900

    try ecs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,13 @@ set -e
 rm -f /app/tmp/pids/server.pid
 # WARNING:createとseedはfargateの初回のみ実行
 # WARNING:タスクを個々に作って実行の方がいいかも
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
+# bundle exec rails db:create
+# bundle exec rails db:migrate
+# bundle exec rails db:seed
+
+rails db:create
+rails db:migrate
+rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
