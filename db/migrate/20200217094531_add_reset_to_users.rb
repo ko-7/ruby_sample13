bf81123a5dc3da377720f23e53a50441c616d25c
shimura <shimura21ngs@gmail.com>
2022-10-15 16:39:16 +0900

    change migratinfile

--- db/migrate/20200217094531_add_reset_to_users.rb
+++ db/migrate/20200217094531_add_reset_to_users.rb
@@ -2,5 +2,6 @@ class AddResetToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :reset_digest, :string
     add_column :users, :reset_sent_at, :datetime
+    add_column :users, :content, :text
   end
 end
--- db/migrate/20220917003256_add_content_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddContentColumnToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :content, :text
-  end
-end
